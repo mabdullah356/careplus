429da84de22a4213eae3f09d8292810058e9af29
mabdullah356 <abdullahworld111@gmail.com>
2026-07-05 12:23:11 +0500

    feat: add login page and integrate react-toastify for notifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,22 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header'
 import HomePage from './components/HomePage'
-import Footer from './components/Footer'  
-function App() {
+import Signup from './Pages/signup'
+import Login from './Pages/login'
+import Footer from './components/Footer'
 
+function App() {
   return (
     <Router>
-
+      <ToastContainer position="top-right" autoClose={3000} />
       <Header />
-    <Routes>
-      <Route path="/" element={<HomePage />} />  
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Footer />
     </Router>
   )
